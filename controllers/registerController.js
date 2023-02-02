bc2385d332a9b11e52baeb1c8c85e4bddc8cc104
Jason Agola <29072707+jasonagola@users.noreply.github.com>
2023-02-02 14:07:41 -0500

    support for user roles

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,14 +2,13 @@ const db = require("../databaseConfig")
 const bcrypt = require('bcrypt')
 
 const registerNewUser = async (req, res) => {
-    // console.log('You have made it to the register controller')
     const {username, password} = req.body
     console.log(username, password)
     if (!username || !password) {
         return res.status(400).json({ 'message': 'Username and Password are required for login'})
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    db.query(`INSERT INTO Users (username, password) VALUES ('${username}', '${hashedPassword}')`, (err, result) => {
+    db.query(`INSERT INTO Users (username, roles, password) VALUES ('${username}', 2001, '${hashedPassword}')`, (err, result) => {
         if (err) {
             console.log(err)
             if(err.errno == 1062) {
